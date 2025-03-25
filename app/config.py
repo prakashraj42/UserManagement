3441f9f3609f57c25029b398548647be02682144
prakashraj42 <prakashraj42mk@gmail.com>
2025-03-25 13:27:40 +0530

    Feat: add security breare

--- app/config.py
+++ app/config.py
@@ -9,5 +9,6 @@ class Settings:
     SECRET_KEY = os.getenv("SECRET_KEY", "!@#$%^&*(123456789)")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
     ALGORITHM  = os.getenv("ALGORITHM", "HS256")
-Settings =Settings()
+
+settings = Settings()
 
